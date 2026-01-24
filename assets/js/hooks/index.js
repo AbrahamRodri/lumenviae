8c26fd366ffb05b2ddcaba29c17a1ac5ab3c68cd
AbrahamRodri <rodriguez.abrahamdev@gmail.com>
2026-01-24 15:14:32 -0600

    Replace localStorage progress tracking with URL-based state management
    Simplifies prayer navigation by using URL query parameters (?mystery=N) as single source of truth instead of localStorage. Enables bookmarking specific mysteries and proper browser back/forward navigation.

--- assets/js/hooks/index.js
+++ assets/js/hooks/index.js
@@ -1,13 +1,11 @@
 import ScrollToTop from "./scroll_to_top"
 import UserTimezone from "./user_timezone"
-import RosaryProgress from "./rosary_progress"
 import AudioPlayer from "./audio_player"
 import MobileMode from "./mobile_mode"
 
 export default {
   ScrollToTop,
   UserTimezone,
-  RosaryProgress,
   AudioPlayer,
   MobileMode
 }
--- lib/lumen_viae_web/live/pray/index.ex
+++ lib/lumen_viae_web/live/pray/index.ex
@@ -22,7 +22,6 @@ defmodule LumenViaeWeb.Live.Pray.Index do
          |> assign(:current_index, 0)
          |> assign(:completion_tracked, false)
          |> assign(:mobile_mode_enabled, false)
-         |> assign(:mobile_mode_preference, "auto")
          |> assign(:page_title, set.name)}
 
       [] ->
@@ -41,8 +40,8 @@ defmodule LumenViaeWeb.Live.Pray.Index do
     new_index = (current + 1) |> clamp_index(total)
 
     socket
-    |> assign_current_meditation(new_index)
     |> maybe_track_completion(new_index)
+    |> push_patch(to: ~p"/meditation-sets/#{socket.assigns.set.id}/pray?mystery=#{new_index}")
     |> then(&{:noreply, &1})
   end
 
@@ -52,58 +51,37 @@ defmodule LumenViaeWeb.Live.Pray.Index do
     new_index = (current - 1) |> clamp_index(total)
 
     socket
-    |> assign_current_meditation(new_index)
+    |> push_patch(to: ~p"/meditation-sets/#{socket.assigns.set.id}/pray?mystery=#{new_index}")
     |> then(&{:noreply, &1})
-  end
-
-  def handle_event("restore_progress", %{"index" => index}, socket) do
-    total = length(socket.assigns.set.meditations)
-    # Ensure index is valid (within bounds)
-    valid_index = index |> normalize_index() |> clamp_index(total)
-    {:noreply, assign_current_meditation(socket, valid_index)}
   end
 
   def handle_event("audio_ended", _params, socket) do
     {:noreply, socket}
   end
 
-  def handle_event("toggle_mobile_mode", %{"preference" => preference}, socket) do
-    enabled =
-      case preference do
-        "on" -> true
-        "off" -> false
-        "auto" -> socket.assigns.mobile_mode_enabled
-      end
-
-    {:noreply,
-     socket
-     |> assign(:mobile_mode_enabled, enabled)
-     |> assign(:mobile_mode_preference, preference)}
+  def handle_event("toggle_mobile_mode", _params, socket) do
+    {:noreply, assign(socket, :mobile_mode_enabled, !socket.assigns.mobile_mode_enabled)}
   end
 
-  def handle_event("init_mobile_mode", %{"enabled" => enabled, "preference" => preference}, socket) do
-    {:noreply,
-     socket
-     |> assign(:mobile_mode_enabled, enabled)
-     |> assign(:mobile_mode_preference, preference)}
-  end
-
-  def handle_event("update_mobile_mode_auto", %{"enabled" => enabled}, socket) do
-    if socket.assigns.mobile_mode_preference == "auto" do
-      {:noreply, assign(socket, :mobile_mode_enabled, enabled)}
-    else
-      {:noreply, socket}
-    end
+  def handle_event("init_mobile_mode", %{"enabled" => enabled}, socket) do
+    {:noreply, assign(socket, :mobile_mode_enabled, enabled)}
   end
 
   @impl true
-  def handle_params(_params, _url, socket) do
+  def handle_params(params, _url, socket) do
     total_count = length(socket.assigns.set.meditations)
+
+    # Read mystery index from URL params, default to current index or 0
+    mystery_index =
+      params
+      |> Map.get("mystery", "0")
+      |> normalize_index()
+      |> clamp_index(total_count)
 
     {:noreply,
      socket
      |> assign(:total_count, total_count)
-     |> assign_current_meditation(socket.assigns.current_index)}
+     |> assign_current_meditation(mystery_index)}
   end
 
   defp assign_current_meditation(socket, index) do
@@ -161,37 +139,4 @@ defmodule LumenViaeWeb.Live.Pray.Index do
   #   end
   # end
 
-  defp cycle_preference("auto"), do: "on"
-  defp cycle_preference("on"), do: "off"
-  defp cycle_preference("off"), do: "auto"
-
-  defp mobile_mode_title("auto"), do: "Mobile Mode: Auto"
-  defp mobile_mode_title("on"), do: "Mobile Mode: On"
-  defp mobile_mode_title("off"), do: "Mobile Mode: Off"
-
-  defp mobile_mode_icon(assigns) do
-    ~H"""
-    <%= case @preference do %>
-      <% "on" -> %>
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-5 h-5">
-          <path d="M10.5 18.75a.75.75 0 000 1.5h3a.75.75 0 000-1.5h-3z" />
-          <path fill-rule="evenodd" d="M8.625.75A3.375 3.375 0 005.25 4.125v15.75a3.375 3.375 0 003.375 3.375h6.75a3.375 3.375 0 003.375-3.375V4.125A3.375 3.375 0 0015.375.75h-6.75zM7.5 4.125C7.5 3.504 8.004 3 8.625 3H9.75v.375c0 .621.504 1.125 1.125 1.125h2.25c.621 0 1.125-.504 1.125-1.125V3h1.125c.621 0 1.125.504 1.125 1.125v15.75c0 .621-.504 1.125-1.125 1.125h-6.75A1.125 1.125 0 017.5 19.875V4.125z" clip-rule="evenodd" />
-        </svg>
-      <% "off" -> %>
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-5 h-5">
-          <path d="M10.5 18.75a.75.75 0 000 1.5h3a.75.75 0 000-1.5h-3z" />
-          <path fill-rule="evenodd" d="M2.25 2.25l19.5 19.5-1.06 1.06L17.56 19.75H15.375a3.375 3.375 0 01-3.375-3.375v-5.25L4.31 3.44 2.25 2.25zm6.375 1.875A3.375 3.375 0 015.25 7.5v12.375a3.375 3.375 0 003.375 3.375h6.75c.73 0 1.397-.232 1.947-.627l-1.572-1.573H8.625A1.125 1.125 0 017.5 19.875V7.5c0-.621.504-1.125 1.125-1.125h6.75c.621 0 1.125.504 1.125 1.125v8.625l2.25 2.25V4.125A3.375 3.375 0 0015.375.75h-6.75z" clip-rule="evenodd" />
-        </svg>
-      <% _ -> %>
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-5 h-5">
-          <g>
-            <path d="M10.5 18.75a.75.75 0 000 1.5h3a.75.75 0 000-1.5h-3z" />
-            <path fill-rule="evenodd" d="M8.625.75A3.375 3.375 0 005.25 4.125v15.75a3.375 3.375 0 003.375 3.375h6.75a3.375 3.375 0 003.375-3.375V4.125A3.375 3.375 0 0015.375.75h-6.75zM7.5 4.125C7.5 3.504 8.004 3 8.625 3H9.75v.375c0 .621.504 1.125 1.125 1.125h2.25c.621 0 1.125-.504 1.125-1.125V3h1.125c.621 0 1.125.504 1.125 1.125v15.75c0 .621-.504 1.125-1.125 1.125h-6.75A1.125 1.125 0 017.5 19.875V4.125z" clip-rule="evenodd" />
-            <circle cx="18" cy="6" r="4" fill="currentColor" />
-            <text x="18" y="8" font-size="6" fill="#1a1a2e" text-anchor="middle" font-weight="bold">A</text>
-          </g>
-        </svg>
-    <% end %>
-    """
-  end
 end
